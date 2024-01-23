d7990fee418ed8ca241b0fd5b67f9229bd4a8573
Antonio Florio <51435958+AntonioF00@users.noreply.github.com>
2024-01-23 15:55:33 +0100

    Update graphs.ts

--- src/controllers/graphs.ts
+++ src/controllers/graphs.ts
@@ -1,17 +1,6 @@
 import express from 'express';
 import { getAllGraphs, createGraph } from '../db/graphs';
 
-export const getGraphs = async (req: express.Request, res: express.Response) => {
-    try{
-
-        const graphs = await getAllGraphs();
-        return res.sendStatus(200).json(graphs);
-
-    }catch(error){
-        console.log(error);
-        return res.status(500).json({message:"Error creating graph"});
-    }
-};
 
 export const createGraphs = async (req: express.Request, res: express.Response) => {
 
@@ -29,5 +18,36 @@ export const createGraphs = async (req: express.Request, res: express.Response) 
         console.log("ERROR IN CREATING GRAPH");
         console.log(error);
         return res.status(400).json({ message : "Failed to create Graph."});
+    }
+};
+
+export const getGraphs = async (req: express.Request, res: express.Response) => {
+    try{
+
+        const graphs = await getAllGraphs();
+        return res.sendStatus(200).json(graphs);
+
+    }catch(error){
+        console.log(error);
+        return res.status(500).json({message:"Error creating graph"});
+    }
+};
+
+export const createNode = async (req: express.Request, res: express.Response) => {
+    try{
+        
+    }catch(error){
+        console.log(error);
+        return res.sendStatus(500).json({message: "Error creating node"});
+    }
+};
+
+
+export const deleteNode = async (req: express.Request, res: express.Response) => {
+    try{
+        
+    }catch(error){
+        console.log(error);
+        return res.sendStatus(500).json({message: "Error deleting node"});
     }
 };
